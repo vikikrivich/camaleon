51fcb064f0b95679864470eed73d95dcfdb5b18b
vikikrivich <levigowik@gmail.com>
2024-03-06 17:17:20 +0600

    init project

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,10 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+
+// Camaleon CMS assets
+//= link camaleon-cms.js
+//= link_tree ../../apps/themes/camaleon_first/assets
+//= link_tree ../../apps/themes/default/assets
+//= link_tree ../../apps/themes/new/assets
